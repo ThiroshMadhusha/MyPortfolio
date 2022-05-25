c75f71f9d74cb4df4277ecc6a7e5d1162d916894
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-05-26 04:40:22 +0530

    Add typing script file

---
+++ MyPortFolio/js/script.js
@@ -0,0 +1,7 @@
+// Typing Animatiion
+var typed = new Typed(".typing", {
+  strings: [" ", "Student", "Web Developer", "Web Designer", "YouTuber"],
+  typeSpeed: 200,
+  BackSpeed: 60,
+  loop: true,
+});
